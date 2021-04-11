f99b81727049bf7e9ee264c676068359ee9af040
Daniel Schmid <danielschmid@Daniels-MacBook-Air.local>
2021-04-11 15:28:38 +0100

    remove package

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -9,7 +9,7 @@ const SEO = ({ title, description }) => {
   return (
     <Helmet htmlAttributes={{ lang: "en" }} title={`${title} | ${siteTitle}`}>
       <meta name="description" content={description || siteDesc} />
-      <script src="https://www.google.com/recaptcha/api.js" defer></script>
+      <script src="https://www.google.com/recaptcha/api.js"></script>
     </Helmet>
   )
 }
