403283bbd1fde03d073ff6ec8071beb80e383729
Daniel Schmid <danielschmid@Daniels-MacBook-Air.local>
2021-05-08 12:18:08 +0100

    fixing contact form errors

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -15,7 +15,7 @@ export default function ContactForm() {
     >
       <div className="flex justify-center" id="contact-form">
         <form
-          action="https://getform.io/f/7204c7bd-76e5-4012-9d3d-dae3c88d2d77"
+          action="https://formspree.io/f/mrgrgdbo"
           method="POST"
           className="bg-primary-base bg-opacity-70 flex flex-col w-11/12 md:w-2/3 max-w-screen-sm md:my-14 px-9 md:px-14 py-6 "
         >
@@ -53,7 +53,7 @@ export default function ContactForm() {
             <input
               type="date"
               name="date"
-              placeholder="Enter your phone number"
+              placeholder="dd-mm-yyy"
               className="form-input"
             />
           </label>
@@ -72,10 +72,6 @@ export default function ContactForm() {
             Message
             <textarea type="text-area" name="message" className="form-input" />
           </label>
-          <div
-            class="g-recaptcha"
-            data-sitekey="6LccgKIaAAAAACXFk1LZTf4h3xEZYR5hjnoRFh33"
-          ></div>
           <button type="submit" className="button-2 mx-auto my-6">
             Send
           </button>
