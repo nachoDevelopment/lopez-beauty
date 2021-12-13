167685682a1c780f56db92572db47e049145a052
Daniel Schmid <daniel@nachodevelopment.uk>
2021-12-13 21:36:36 +0000

    added dubsado contact form

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,96 +1,31 @@
-import { graphql, useStaticQuery } from "gatsby"
 import React from "react"
 
-import BackgroundImage from "gatsby-background-image"
-
 export default function ContactForm() {
-  const data = useStaticQuery(query)
-  const formBg =
-    data.strapiContact.contact_form_background.childImageSharp.fluid
+  const dubsadoForm = `
+  <div style="width: 90%">
+  <script
+  type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.14/iframeResizer.min.js"
+></script>
+<iframe
+  src="https://hello.dubsado.com:443/public/form/view/61afaaa68efb95b42c1392ef"
+  ;
+  frameborder="0"
+  style="width: 100%; min-width: 100%; height: 850px"
+></iframe>
+<script type="text/javascript">
+  setTimeout(function () {
+    iFrameResize({checkOrigin: false, heightCalculationMethod: 'taggedElement'})
+  }, 30)
+</script>
+</div>
+  `
 
   return (
-    <BackgroundImage
-      className="bg-center bg-cover bg-no-repeat bg-fixed px-6 py-10 md:p-0"
-      fluid={formBg}
-    >
-      <div className="flex justify-center" id="contact-form">
-        <form
-          action="https://formspree.io/f/mrgrgdbo"
-          method="POST"
-          className="bg-primary-base bg-opacity-70 flex flex-col w-11/12 md:w-2/3 max-w-screen-sm md:my-14 px-9 md:px-14 py-6 "
-        >
-          <label htmlFor="name" className="form-label">
-            Name*
-            <input
-              type="text"
-              name="name"
-              placeholder="Enter your full name"
-              className="form-input text-p"
-              required
-            />
-          </label>
-          <label htmlFor="email" className="form-label">
-            Email*
-            <input
-              type="email"
-              name="email"
-              placeholder="Enter your email"
-              className="form-input"
-              required
-            />
-          </label>
-          <label htmlFor="number" className="form-label">
-            Number
-            <input
-              type="text"
-              name="number"
-              placeholder="Enter your phone number"
-              className="form-input"
-            />
-          </label>
-          <label htmlFor="date" className="form-label">
-            Event Date
-            <input
-              type="date"
-              name="date"
-              placeholder="dd-mm-yyy"
-              className="form-input"
-            />
-          </label>
-          <label htmlFor="eventType" className="form-label">
-            Event Type*
-            <select name="event" className="form-input" required>
-              <option value="wedding">Wedding</option>
-              <option value="special Occasion">Special Occasion</option>
-              <option value="party">Party</option>
-              <option value="photoshoot">Photoshoot</option>
-              <option value="lesson">Lesson</option>
-              <option value="brows">Brows</option>
-            </select>
-          </label>
-          <label htmlFor="message" className="form-label">
-            Message
-            <textarea type="text-area" name="message" className="form-input" />
-          </label>
-          <button type="submit" className="button-2 mx-auto my-6">
-            Send
-          </button>
-        </form>
-      </div>
-    </BackgroundImage>
+    <div
+      className="flex justify-center bg-primary-base px-6 py-10 md:p-0"
+      id="contact-form"
+      dangerouslySetInnerHTML={{ __html: dubsadoForm }}
+    ></div>
   )
 }
-
-const query = graphql`
-  query {
-    strapiContact {
-      contact_form_background {
-        childImageSharp {
-          fluid(maxWidth: 1800) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-    }
-  }
-`
